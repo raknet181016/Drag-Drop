977c418c60c32c17893f6d3f6dfa11e78a61602d
raknet <mirvahidkanaatov744@gmail.com>
2022-07-09 02:01:55 +0800

    end

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
 const item = document.querySelector(".item");
+const placeholders = document.querySelectorAll(".placeholder");
 
 item.addEventListener("dragstart", itemDragstart);
 item.addEventListener("dragend", itemDragend);
 
-function itemDragstart() {}
+for (const placeholder of placeholders) {
+    placeholder.addEventListener("dragover", placeholderDragover);
+    placeholder.addEventListener("dragenter", placeholderDragenter);
+    placeholder.addEventListener("dragleave", placeholderDragleave);
+    placeholder.addEventListener("drop", placeholderDrop);
+}
 
-function itemDragend() {}
+function itemDragstart(e) {
+    e.target.classList.add("hold");
+    setTimeout(() => e.target.classList.add("hide"), 0);
+}
+
+function itemDragend(e) {
+    e.target.classList.remove("hold", "hide");
+}
+
+function placeholderDragover(e) {
+    e.preventDefault();
+}
+
+function placeholderDragenter(e) {
+    e.target.classList.add("hovered");
+}
+
+function placeholderDragleave(e) {
+    e.target.classList.remove("hovered");
+}
+
+function placeholderDrop(e) {
+    e.target.classList.remove("hovered");
+    e.target.append(item);
+}
