1b7c8e3f248b7297aad12c55fc815cc7bf93a4eb
raknet <mirvahidkanaatov744@gmail.com>
2022-07-09 01:32:23 +0800

    second commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const item = document.querySelector(".item");
+
+item.addEventListener("dragstart", itemDragstart);
+item.addEventListener("dragend", itemDragend);
+
+function itemDragstart() {}
+
+function itemDragend() {}
